2b050d54f86ee30f7e7a7746ad209520b88134f4
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2022-12-30 17:26:49 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Calculator from './components/calculadora';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
